dc5be287808382c66f628a578ce925c297e166d3
sarakvistgaard <sawa1001@stud.ek.dk>
2026-06-08 16:20:31 +0200

    second commit eksamen

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 /**************** TEMA 2 ***************/
 document.querySelector("#link_pil").textContent = ">";
+
+// link scale
+document.querySelector(".link_og_pil").addEventListener("mouseover", link_mouseover);
+
+function link_mouseover() {
+  console.log("test");
+  document.querySelector(".link_og_pil").classList.add("scale_link");
+}
+
+document.querySelector(".link_og_pil").addEventListener("mouseout", no_scale_link);
+
+function no_scale_link() {
+  document.querySelector(".link_og_pil").classList.remove("scale_link");
+}
+
+/**************** PROJEKTER ***************/
